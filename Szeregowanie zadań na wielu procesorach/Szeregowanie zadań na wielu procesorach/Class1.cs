72774da271c75cb3f9f717b930e47a37005e603a
derdaster <derdaster@gmail.com>
2014-05-12 22:49:50 +0200

    Wstępny projekt

---
+++ Szeregowanie zadań na wielu procesorach/Szeregowanie zadań na wielu procesorach/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szeregowanie_zadań_na_wielu_procesorach
+{
+    class Processor
+    {
+        Task procTask;
+        public void chooseAlgoritm()
+        {
+        }
+        public void symulation()
+        {
+        }
+
+    }
+    class Task
+    {
+        bool isDone;
+        int time;
+        int priority;
+    }
+}
---
+++ Szeregowanie zadań na wielu procesorach/Szeregowanie zadań na wielu procesorach/SchedulerAlgorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szeregowanie_zadań_na_wielu_procesorach
+{
+    interface SchedulerAlgorithm
+    {
+        public void algorithm();
+    }
+
+    class Algorithm1 : SchedulerAlgorithm
+    {
+
+    }
+
+    class Core
+    {
+        public void chooseAlgorithm();
+        public void process();
+        public void addObserver();
+        public void removeObserver();
+        public void actualiseObservers();
+    }
+    interface SchedulerAlgorithm
+    {
+        public void addObserver();
+        public void removeObserver();
+        public void actualiseObservers();
+    }
+}
